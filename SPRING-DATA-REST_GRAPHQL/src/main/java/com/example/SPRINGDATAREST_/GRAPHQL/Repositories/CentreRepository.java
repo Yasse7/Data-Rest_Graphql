1e69bb3746cc433c9b00a62bf5bb387a4765b724
Yasse7 <jerrariyasser2001@gmail.com>
2023-12-10 00:18:35 +0100

    commit

---
+++ SPRING-DATA-REST_GRAPHQL/src/main/java/com/example/SPRINGDATAREST_/GRAPHQL/Repositories/CentreRepository.java
@@ -0,0 +1,8 @@
+package com.example.SPRINGDATAREST_.GRAPHQL.Repositories;
+
+import com.example.SPRINGDATAREST_.GRAPHQL.entities.Centre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CentreRepository  extends JpaRepository<Centre,Long> {
+
+}
